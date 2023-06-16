6b1131cae28a1ba9b7e5870f2bfedaec498645f0
joahJS <91927771+joahJS@users.noreply.github.com>
2023-06-16 16:12:02 +0900

    111

--- dist/assets/Abouts.e56f043c.js
+++
@@ -1,1 +0,0 @@
-import{s as a}from"./subPageHero.62d03af5.js";import{o,c as s,a as t,F as r,b as e}from"./index.bc58fcb6.js";const c=e("div",null,[e("h1",null,"\uD68C\uC0AC\uC18C\uAC1C")],-1),p={__name:"Abouts",setup(n){return(l,_)=>(o(),s(r,null,[t(a),c],64))}};export{p as default};
